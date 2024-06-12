0fa0869a17220c0fe2220afa473a856c48a9d0c5
Matheus <147951308+mparreiracruz@users.noreply.github.com>
2024-06-12 12:25:18 -0300

    Update 03_Importando_um_Dataset.py

--- 03_Importando_um_Dataset.py
+++ 03_Importando_um_Dataset.py
@@ -2,58 +2,26 @@ import pandas as pd
 #carregando o dataset corretamente ==> neste caso, use o separdor ';'
 data = pd.read_csv('/Users/matheus/Downloads/GasPricesinBrazil_2004-2019.csv', sep=';')
 
+def print_full(df):
+    # Salvando as configurações atuais
+    pd.set_option('display.max_rows', None)  # Exibir todas as linhas
+    pd.set_option('display.max_columns', None)  # Exibir todas as colunas
+    pd.set_option('display.max_colwidth', None)  # Exibir conteúdo completo das colunas
+    pd.set_option('display.width', None)  # Ajustar largura de exibição
+
+    # Imprimindo o DataFrame
+    print(df)
+
+    # Restaurando as configurações padrão após a exibição
+    pd.reset_option('display.max_rows')
+    pd.reset_option('display.max_columns')
+    pd.reset_option('display.max_colwidth')
+    pd.reset_option('display.width')
+
+print_full(data.head())
+
 #print(data)
+
 #print(data.head())
 #print(data.info())
-#print(type(data))
-#print(data.shape)
-# print(f'O DataFrame possui {data.shape[0]} linhas/observações/registros e {data.shape[1]} colunas/atributos/variáveis.')
 
-
-# personagens_df = pd.DataFrame({
-# 'nome': ['Luke Skywalker', 'Yoda', 'Palpatine'],
-# 'idade': [16, 1000, 70],
-# 'peso' : [70.5, 15.2, 60.1],
-# 'eh Jedi' :[True, True, False]
-# })
-
-
-#print(personagens_df)
-#print(personagens_df.info())
-#print(personagens_df.columns)
-#print(type(personagens_df.columns))
-#print(list(personagens_df.columns))
-
-#print(personagens_df)
-# personagens_df_renomeado = personagens_df.rename(columns={
-#     'nome': 'Nome Completo',
-#     'idade': 'Idade'
-# })
-
-#print(personagens_df_renomeado)
-# personagens_df.rename(columns={'nome': 'Nome Completo','idade': 'Idade'}, inplace=True)
-#
-# print(personagens_df.columns)
-#
-# personagens_df.columns = ['NOME', 'IDADE', 'PESO', 'EH JEDI']
-#
-# print(personagens_df)
-
-# print(data.head())
-#print(data['ESTADO'])
-
-#print(data.ESTADO)
-
-#print(type(data['ESTADO']))
-
-#print(data.iloc[1])
-
-#print(type(data.iloc[1]))
-
-#print(pd.Series([5.5, 6.0, 9.5]))
-
-#print(pd.Series([5.5, 6.0, 9.5], index=['prova 1', 'prova 2', 'projeto'], name='Notas do Luke Skywalker'))
-
-#print(data)
-
-print(data['PRODUTO'])
